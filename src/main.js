6932fcd54b939bd2c1bd25eb77455fb8f3545b34
cooper-runstein <cooperun@gmail.com>
2018-07-23 16:53:17 -0700

    Update 7/23

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ if (isIE){
 var work = new Vue({
     el: "#work",
     data: {
-        message: "Hello World",
         projects: [
-            { name: "First React Project -->  ", src: 'https://github.com/Cooper-Runstein/NBA-App-Learn-React/tree/6984cc8b19e412b51d5d2299862ce2e8750e9067', id: 1, projectTitle: "NBA Stat Comparison" },
-            { name: "First AJAX Project -->  ", src: 'https://github.com/Cooper-Runstein/MLB-Players-App', id: 2, projectTitle: "MLB Player Search App" },
-            { name: "This Website's code -->  ", src: 'https://github.com/Cooper-Runstein/Website-CooperRunstein',id: 5, projectTitle: "CooperRunstein.com"},
-            { name: "First Big Javascript Project -->  ", src: 'https://github.com/Cooper-Runstein/Liars-dice', id: 3, projectTitle: "Liar's Dice Game" },
-            { name: "First Big Python Project -->  ", src: 'https://github.com/Cooper-Runstein/Python-Payment-Determination-App', id: 4, projectTitle: "Roomate Payments App" }
+            { name: "React Personal Finance App -->  ", src: 'https://github.com/Cooper-Runstein/React-Personal-Finance-App', id: 1, projectTitle: "Personal Finance App"},
+            { name: "First React Project -->  ", src: 'https://github.com/Cooper-Runstein/NBA-App-Learn-React/tree/6984cc8b19e412b51d5d2299862ce2e8750e9067', id: 2, projectTitle: "NBA Stat Comparison" },
+            { name: "First AJAX Project -->  ", src: 'https://github.com/Cooper-Runstein/MLB-Players-App', id: 3, projectTitle: "MLB Player Search App" },
+            { name: "This Website's code -->  ", src: 'https://github.com/Cooper-Runstein/Website-CooperRunstein',id: 6, projectTitle: "CooperRunstein.com"},
+            { name: "First Big Javascript Project -->  ", src: 'https://github.com/Cooper-Runstein/Liars-dice', id: 4, projectTitle: "Liar's Dice Game" },
+            { name: "First Big Python Project -->  ", src: 'https://github.com/Cooper-Runstein/Python-Payment-Determination-App', id: 5, projectTitle: "Roomate Payments App" }
         ],
     }
 })
@@ -24,4 +24,17 @@ var skills = new Vue({
     data: {
         show: 1,
     }
+})
+
+VueScrollTo.setDefaults({
+    container: "body",
+    duration: 500,
+    easing: "ease",
+    offset: 0,
+    cancelable: true,
+    onStart: false,
+    onDone: false,
+    onCancel: false,
+    x: false,
+    y: true
 })
