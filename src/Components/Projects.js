2437ed2af0bc5a1bd16c9921d9ab37a92b807572
cooper-runstein <cooperun@gmail.com>
2018-08-20 01:37:27 -0700

    Text updates

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,7 +11,7 @@ const Project = (props) =>{
       <h4>{props.name}</h4>
       <div className='project-links'>
         <a href={props.src} className='code-link'> {'{ get the code }'}  </a>
-        {props.live ? <a href={props.Live} className='live-link'>view it live</a> : null}
+        {props.live ? <a href={props.live} className='live-link'>view it live</a> : null}
       </div>
 
       <p>{props.desciription}</p>
@@ -25,50 +25,64 @@ class Projects extends React.Component{
     this.state = {
       projects: [
         {
+          name: "React Router Example",
+          src: 'https://github.com/Cooper-Runstein/Reactify-Coffee-Shop',
+          projectTitle: "Coffee Shop Application",
+          live: "https://boring-hypatia-eb85d2.netlify.com/menu/coffee",
+          desciription: "This is a Single Page Application ceated as a mock website when I was learning CSS, I modified it when learning React Router."
+        },
+        {
           name: "Node/Express server and a React frontend",
           projectTitle: "SpotCheck Admin Tools",
           src: 'https://bitbucket.org/spotcheckparkingsolutions/admin-tools/src/master/',
           desciription: "A set of administration tools to change the status of remote raspberry pi clients that run moterized vehicle detecion nueral networks. Uses the React Context API, React Router, and JSON web tokens to create a secure system for parking detection administration."
         },
         {
+          name: "Vue Router based SPA",
+          projectTitle: "SpotCheckParking.com",
+          src: '',
+          live: 'http://spotcheckparking.com/',
+          desciription: "The official SpotCheck Parking Solutions website, still under construction."
+        },
+        {
           name: "React Project",
           src: 'https://github.com/Cooper-Runstein/React-Personal-Finance-App',
           projectTitle: "Personal Finance App",
-          // live: "https://boring-hypatia-eb85d2.netlify.com/menu/coffee",
           desciription: "In progress application that creates a chart extending from now unitl a given retirement day with one's financial status."
-        },
-        {
-          name: "React Router Example",
-          src: 'https://github.com/Cooper-Runstein/Reactify-Coffee-Shop',
-          projectTitle: "Coffee Shop Application",
-          live: "https://boring-hypatia-eb85d2.netlify.com/menu/coffee",
-          desciription: "This is a Single Page Application ceated as a mock website when learning CSS, I modified it when learning React Router."
         },
         {
           name: "First React Project",
           src: 'https://github.com/Cooper-Runstein/NBA-App-Learn-React/tree/6984cc8b19e412b51d5d2299862ce2e8750e9067',
           projectTitle: "NBA Stat Comparison",
-          desciription: "This project uses the mySportsFeed API to get NBA player's statistics and compare them to other players. The project is built with React."
+          live: 'https://quirky-meninsky-9fb61f.netlify.com/',
+          desciription: "This project uses the mySportsFeed API to get NBA player's statistics and compare them to other players. The project is one of the first I built with React that wasn't following a tutorial."
         },
         {
           name: "First AJAX Project",
           src: 'https://github.com/Cooper-Runstein/MLB-Players-App',
-          projectTitle: "MLB Player Search App"
+          projectTitle: "MLB Player Search App",
+          live: 'https://quizzical-euler-a68aff.netlify.com/',
+          desciription: "This project uses the mySportsFeed API to get MLB player data. It was also one of the first projects I used CSS transitions and transforms in for more than a single feature."
         },
         {
           name: "This Website's code",
           src: 'https://github.com/Cooper-Runstein/Website-CooperRunstein',
-          projectTitle: "CooperRunstein.com"
+          live: '#',
+          projectTitle: "CooperRunstein.com",
+          desciription: 'This is probably the 4th iteration of personal website I\'ve desigined. I\'ve intentionally kept the styles minimal to not get boggd down on a site I\'m sure will be redone in a few months.'
+
         },
         {
           name: "First Big Javascript Project",
           src: 'https://github.com/Cooper-Runstein/Liars-dice',
-          projectTitle: "Liar's Dice Game"
+          projectTitle: "Liar's Dice Game",
+          desciription: "This was one of the first DOM manipulation projects I built. It isn't very special anymore, but I put a lot of work into trying to make this work."
         },
         {
           name: "First Big Python Project",
           src: 'https://github.com/Cooper-Runstein/Python-Payment-Determination-App',
-          projectTitle: "Roomate Payments App"
+          projectTitle: "Roomate Payments App",
+          desciription: 'This projects was originally built in fall of 2017 as the first "full" program I felt worthy of putting on my github when I signed up.'
         }
     ],
     activeIndex: 0
@@ -93,12 +107,13 @@ class Projects extends React.Component{
               this.setState({activeIndex: newIndex})
             }}></img>
 
-            <ReactCSSTransitionGroup
+            {/* <ReactCSSTransitionGroup
               component = "span"
               transitionName = "projectDisplay"
-              transitionEnterTimeout= {500}
-              transitionLeaveTimeout= {500}
-            >
+              // transitionEnterTimeout= {500}
+              // transitionLeaveTimeout= {500}
+            > */}
+            <span>
               {
                 this.state.projects.map((project, index)=>{
                   if (this.state.activeIndex === index){
@@ -118,7 +133,8 @@ class Projects extends React.Component{
 
                 })
               }
-            </ReactCSSTransitionGroup>
+              </span>
+            {/* </ReactCSSTransitionGroup> */}
 
             <img
               src={forwardArrow}
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -22,8 +22,8 @@ class Skills extends React.Component {
             title: 'CSS and HTML',
             lines: [
               'I began learning CSS and HTML in fall 2017 as a result of trying Android development and realizing I enjoyed the UI aspect.',
-              'After learning native CSS I experimented with Bootstrap, Flexbox, Grid.',
-              'I mostly use a combonation of flex and grid today, these more modern tools ',
+              'After learning native CSS I experimented with Bootstrap, Flexbox, and Grid.',
+              'I\'ve learned about the mobile first approach to web design and aim to make my projects work on screens of all sizes.',
               'Recently I\'ve picked up SASS basics to help build my style sheets quickly.'
             ]
           },
@@ -32,7 +32,16 @@ class Skills extends React.Component {
             lines: [
               'I began learning Javascript around the same time as HTML and CSS and it quickly overtook Python as my favorite langauge.',
               'I\'ve began using React and Vue as frotend technologies while experimenting with Node as a backend.',
-              'Tensorflow.js has made my affection for JS even stronger and I have a few web-based machine learning projects planned out.',
+              'Machine learning in Python initially set me on the programming track, and I\'m excited to try tensorflow.js in upcoming personal projects.'
+            ]
+          },
+          {
+            title: 'React',
+            lines: [
+                  'At the suggestion of my father, a software engineer, I began working with React in March of 2018.',
+                  'Since then I\'ve built several projects with it and truly love working with it.',
+                  'I\'ve used React Router, React Transition Groups, some Redux.',
+                  'I recently used the React Context API and am excited to explore the options it provides in the future.'
             ]
           },
           {
@@ -47,20 +56,21 @@ class Skills extends React.Component {
           {
             title: 'Database Querying',
             lines: [
-                'I learned SQL alongside python while doing an economic research project, it was certainly not my favorite first time through.',
-                'Since then I\'ve grown to appreciate SQL and the power it offers.',
-                'Recently I took a course on the NoSql technology MongoDB and found it easy and fun to use. ',
+                'Alongside Django and Node, I\'ve learned to use SQL to get and store data in databases.',
+                'Since first using it, I\'ve grown to appreciate SQL and the power it offers.',
+                'I\'ve used both PostgreSQL and MySQL',
+                'Recently I took a course on MongoDB and found it easy and fun to use. ',
               ]
           },
 
           {
             title: 'Developer Tools',
             lines: [
-                'Once I discoverd the ability to undo mistakes with Git, I began using it on every project almost daily.',
+                  'Once I discoverd the ability to undo mistakes with Git, I began using it on every project almost daily.',
                   'Gulp, Webpack, Browserify, Babel and many more libraries and tools have helped me manage and grow my projects.',
                   'Pipenv and NPM for python and JS respectively have made sharing and managing projects much easier.',
                   'I\'ve used numerous editors and IDEs, Atom, Webstorm and PyCharm to name a few. Today I mostly enjoy using Visual Studio Code.',
-                  'I mostly use Linux as my operating system, and have worked through the linux from scratch manual with a friend.',
+                  'I mostly use Linux as my operating system, and spent a few days working through the linux from scratch manual with a friend.',
             ]
           }
         ]
